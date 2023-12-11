1464320d9ea06dce6060e6f0ddf3b18efa82ce42
TEDKORDEV <ted.kordev@gmail.com>
2023-12-11 20:23:30 +0900

    lottie ani added

--- src/screens/onboarding-screens/childrens/onboarding-screen.tsx
+++ src/screens/onboarding-screens/childrens/onboarding-screen.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
 import Container from '../../../components/common/container';
 import { Image } from 'react-native-magnus';
 import Onboarding from 'react-native-onboarding-swiper';
+import LottieView from 'lottie-react-native';
 
+const { width, height } = Dimensions.get('window');
 export default function OnbordingScreen() {
   return (
     <Container>
-      <Text></Text>
       <Onboarding
         pages={[
           {
             backgroundColor: '#fff',
             image: (
-              <View>
-                <Text>Onboarding</Text>
+              <View style={styles.lottie}>
+                <LottieView
+                  style={{ flex: 1 }}
+                  source={require('../../../Assets/animations/boost.json')}
+                  autoPlay
+                  loop
+                />
               </View>
             ),
             title: 'Onboarding',
@@ -23,8 +29,13 @@ export default function OnbordingScreen() {
           {
             backgroundColor: '#fff',
             image: (
-              <View>
-                <Text>Onboarding</Text>
+              <View style={styles.lottie}>
+                <LottieView
+                  style={{ flex: 1 }}
+                  source={require('../../../Assets/animations/boost4.json')}
+                  autoPlay
+                  loop
+                />
               </View>
             ),
             title: 'Onboarding',
@@ -33,8 +44,28 @@ export default function OnbordingScreen() {
           {
             backgroundColor: '#fff',
             image: (
-              <View>
-                <Text>Onboarding</Text>
+              <View style={styles.lottie}>
+                <LottieView
+                  style={{ flex: 1 }}
+                  source={require('../../../Assets/animations/boost3.json')}
+                  autoPlay
+                  loop
+                />
+              </View>
+            ),
+            title: 'Onboarding',
+            subtitle: 'Done with React Native Onboarding Swiper',
+          },
+          {
+            backgroundColor: '#fff',
+            image: (
+              <View style={styles.lottie}>
+                <LottieView
+                  style={{ flex: 1 }}
+                  source={require('../../../Assets/animations/boost5.json')}
+                  autoPlay
+                  loop
+                />
               </View>
             ),
             title: 'Onboarding',
@@ -45,3 +76,11 @@ export default function OnbordingScreen() {
     </Container>
   );
 }
+
+const styles = {
+  lottie: {
+    width: width * 0.8,
+    height: height * 0.4,
+    backgroundColor: '#fff',
+  },
+};
